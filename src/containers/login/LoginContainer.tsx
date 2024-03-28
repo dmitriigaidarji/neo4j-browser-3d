1742c0be5f00a39e0355a23898c6f768850e90df
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2024-03-28 21:55:14 +0800

    add login

---
+++ src/containers/login/LoginContainer.tsx
@@ -0,0 +1,15 @@
+
+function LoginContainer(){
+    return <div>
+        <form className={'form'}>
+            <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input"/>
+                </div>
+            </div>
+        </form>
+    </div>
+}
+
+export default LoginContainer
