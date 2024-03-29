2fc298d15289efda1c22ae565edd542e88a5fa5f
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2024-03-29 22:10:09 +0800

    show node text

--- src/containers/graph/GraphContainer.tsx
+++ src/containers/graph/GraphContainer.tsx
@@ -1,26 +1,53 @@
 import { useEffect, useMemo, useRef } from "react";
-import { IGraph, INode } from "./helpers";
+import { IGraph, ILink, INode } from "./helpers";
 import ForceGraph3D from "3d-force-graph";
 import { cloneDeep } from "lodash-es";
+import {
+  CSS2DRenderer,
+  CSS2DObject,
+} from "three/addons/renderers/CSS2DRenderer.js";
 
 function GraphContainer({ graph }: { graph: IGraph }) {
   const graphDomRef = useRef<HTMLDivElement>(null);
   const graphInstance = useMemo(
     () =>
-      ForceGraph3D()
+      ForceGraph3D({
+        extraRenderers: [new CSS2DRenderer() as unknown as any],
+      })
         .nodeId("elementId")
         .linkSource("startNodeElementId")
         .linkTarget("endNodeElementId")
         .nodeAutoColorBy((t) => {
           return (t as unknown as INode).labels.join(",");
-        }),
+        })
+        .linkAutoColorBy((t) => (t as unknown as ILink).type)
+        .linkWidth(1)
+        .nodeResolution(16)
+        .linkDirectionalArrowLength(3.5)
+        .linkDirectionalArrowRelPos(1)
+        .linkCurvature(0.05)
+        .nodeThreeObject((node) => {
+          const nodeEl = document.createElement("div");
+          const properties = (node as any).properties;
+          nodeEl.textContent =
+            properties.name ?? properties.title ?? (node as any).elementId;
+          nodeEl.style.color = (node as unknown as any).color ?? "";
+          nodeEl.className = "node-label";
+          return new CSS2DObject(nodeEl);
+        })
+        .nodeThreeObjectExtend(true),
     [],
   );
   useEffect(() => {
     if (graphDomRef.current) {
       graphInstance(graphDomRef.current)
         .width(graphDomRef.current.getBoundingClientRect().width)
-        .height(graphDomRef.current.getBoundingClientRect().height - 200);
+        .height(
+          Math.max(
+            600,
+            graphDomRef.current.getBoundingClientRect().height - 200,
+          ),
+        );
     }
     return function cleanup() {
       const r = graphInstance.renderer();
